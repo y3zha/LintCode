6214bea13de66786f93aa07855a5cb568ca7f985
zhangye <yzhang0711@163.com>
2020-01-24 23:54:48 +0800

    二分查找

---
+++ binarysearch/src/_458_target最后一次出现的位置.java
@@ -0,0 +1,28 @@
+public class _458_target最后一次出现的位置 {
+
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                start = mid;
+            } else if (nums[mid] > target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            }
+        }
+        //double check
+        if (nums[end] == target) {
+            return end;
+        }
+        if (nums[start] == target) {
+            return start;
+        }
+        return -1;
+    }
+}
