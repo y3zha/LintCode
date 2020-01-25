4e8881203ab7d2c0ef9a3f29a969a78d2173db88
zhangye <yzhang0711@163.com>
2020-01-25 22:12:47 +0800

    二分查找

---
+++ dfs/src/_015_全排列.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _015_全排列 {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            res.add(new ArrayList<>());
+            return res;
+        }
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, new ArrayList<Integer>(), res, visited);
+        return res;
+    }
+
+    private void dfs(int[] nums, ArrayList<Integer> list, List<List<Integer>> res, boolean[] visited) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(nums[i]);
+                dfs(nums, list, res, visited);
+                list.remove(list.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
